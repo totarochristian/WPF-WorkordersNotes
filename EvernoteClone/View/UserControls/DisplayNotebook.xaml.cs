4fb171d01f41c5fa691bbb7a429e541c46812593
totarochristian <totaro.christian.18@gmail.com>
2023-11-07 23:23:46 +0100

    Update DisplayNotebook, add the notebook dependency property

--- EvernoteClone/View/UserControls/DisplayNotebook.xaml.cs
+++ EvernoteClone/View/UserControls/DisplayNotebook.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EvernoteClone.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,6 +21,27 @@ namespace EvernoteClone.View.UserControls
     /// </summary>
     public partial class DisplayNotebook : UserControl
     {
+        public Notebook Notebook
+        {
+            get { return (Notebook)GetValue(NotebookProperty); }
+            set { SetValue(NotebookProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty NotebookProperty =
+            DependencyProperty.Register("Notebook", typeof(Notebook), typeof(DisplayNotebook), new PropertyMetadata(null, SetValues));
+
+        private static void SetValues(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            //Cast the dependency object to this type of user control
+            DisplayNotebook notebookUserControl = d as DisplayNotebook;
+            //When change the property, re-assign it to the user control if is not null
+            if(notebookUserControl != null)
+            {
+                notebookUserControl.DataContext = notebookUserControl.Notebook;
+            }
+        }
+
         public DisplayNotebook()
         {
             InitializeComponent();
