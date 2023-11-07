1d0b704385f12baf085a935a38c60e99c00d8ea2
totarochristian <totaro.christian.18@gmail.com>
2023-11-07 10:27:01 +0100

    Update database helper class, add the static db file location. This variable must to be static because his value is ever the same, in all the instances of this class.

--- EvernoteClone/ViewModel/Helpers/DatabaseHelper.cs
+++ EvernoteClone/ViewModel/Helpers/DatabaseHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,6 @@ namespace EvernoteClone.ViewModel.Helpers
 {
     class DatabaseHelper
     {
-
+        private static string dbFile = Path.Combine(Environment.CurrentDirectory, "notesDb.db3");
     }
 }
