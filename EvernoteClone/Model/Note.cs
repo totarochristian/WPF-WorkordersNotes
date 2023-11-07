ab8b4c633eecfc162229c1525b7475797cf7b750
totarochristian <totaro.christian.18@gmail.com>
2023-11-07 10:17:36 +0100

    Add primary key and auto increment tag to param Id of the classes: Note, Notebook and User

--- EvernoteClone/Model/Note.cs
+++ EvernoteClone/Model/Note.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,7 @@ namespace EvernoteClone.Model
 {
     class Note
     {
+        [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
         public int NotebookId { get; set; }
         public string Title { get; set; }
--- EvernoteClone/Model/User.cs
+++ EvernoteClone/Model/User.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SQLite;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,7 @@ namespace EvernoteClone.Model
 {
     class User
     {
+        [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Lastname { get; set; }
