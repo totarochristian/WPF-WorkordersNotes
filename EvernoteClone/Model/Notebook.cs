d202b7b85030025ac688a14405d3765b911f6701
totarochristian <totaro.christian.18@gmail.com>
2023-11-07 10:19:59 +0100

    Update Notebook class, set the field UserId to Indexed (sql tag)

--- EvernoteClone/Model/Notebook.cs
+++ EvernoteClone/Model/Notebook.cs
@@ -11,6 +11,7 @@ namespace EvernoteClone.Model
     {
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
+        [Indexed]
         public int UserId { get; set; }
         public string Name { get; set; }
     }
