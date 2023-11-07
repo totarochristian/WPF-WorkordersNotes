03dd128bd381ac521231140c370471ef8199a81b
totarochristian <totaro.christian.18@gmail.com>
2023-11-07 11:14:33 +0100

    Update RegisterCommand class, remove exception in the execute method, add comment with to do

--- EvernoteClone/ViewModel/Commands/RegisterCommand.cs
+++ EvernoteClone/ViewModel/Commands/RegisterCommand.cs
@@ -19,7 +19,7 @@ namespace EvernoteClone.ViewModel.Commands
 
         public void Execute(object? parameter)
         {
-            throw new NotImplementedException();
+            //TODO: Login functionality
         }
     }
 }
