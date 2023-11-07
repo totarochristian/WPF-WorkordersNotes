82629a78057a5f2b36cecef83f9a3d8007fa136a
totarochristian <totaro.christian.18@gmail.com>
2023-11-07 13:40:04 +0100

    Update NewNotebookCommand, add constructor with notes vm param

--- EvernoteClone/ViewModel/Commands/NewNotebookCommand.cs
+++ EvernoteClone/ViewModel/Commands/NewNotebookCommand.cs
@@ -12,6 +12,11 @@ namespace EvernoteClone.ViewModel.Commands
         public NotesVM VM { get; set; }
         public event EventHandler? CanExecuteChanged;
 
+        public NewNotebookCommand(NotesVM vm)
+        {
+            VM = vm;
+        }
+
         public bool CanExecute(object? parameter)
         {
             return true;
