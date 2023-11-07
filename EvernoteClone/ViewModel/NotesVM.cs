df216155b1f78a2f13ad250ea55a46d7b9187623
totarochristian <totaro.christian.18@gmail.com>
2023-11-07 13:28:55 +0100

    Update NotesVM, add Notebooks property ad ObservableCollection of Notebook objects

--- EvernoteClone/ViewModel/NotesVM.cs
+++ EvernoteClone/ViewModel/NotesVM.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using EvernoteClone.Model;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,7 @@ namespace EvernoteClone.ViewModel
 {
     public class NotesVM
     {
+        public ObservableCollection<Notebook> Notebooks { get; set; }
+
     }
 }
