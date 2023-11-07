d2d0188211cfcdf848850aac7fe8b5d4601bfa0c
totarochristian <totaro.christian.18@gmail.com>
2023-11-07 10:19:04 +0100

    Update User class, set Name and Lastname max length (sql tag)

--- EvernoteClone/Model/User.cs
+++ EvernoteClone/Model/User.cs
@@ -11,7 +11,9 @@ namespace EvernoteClone.Model
     {
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
+        [MaxLength(50)]
         public string Name { get; set; }
+        [MaxLength(50)]
         public string Lastname { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
