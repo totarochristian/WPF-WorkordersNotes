c549898bc91717fa20248d54bb8c729378cfab1f
totarochristian <totaro.christian.18@gmail.com>
2023-11-08 17:35:12 +0100

    Remove sqlite tags from Note class

--- EvernoteClone/Model/Note.cs
+++ EvernoteClone/Model/Note.cs
@@ -9,9 +9,7 @@ namespace EvernoteClone.Model
 {
     public class Note
     {
-        [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        [Indexed]
         public int NotebookId { get; set; }
         public string Title { get; set; }
         public DateTime CreatedAt { get; set; }
