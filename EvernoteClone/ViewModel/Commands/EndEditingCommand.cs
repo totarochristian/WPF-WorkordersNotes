aceed9dd7ff61ff4d9ebe62ac83c1821e29353b2
totarochristian <totaro.christian.18@gmail.com>
2023-11-08 09:10:54 +0100

    Update EndEditingCommand, implement interface

--- EvernoteClone/ViewModel/Commands/EndEditingCommand.cs
+++ EvernoteClone/ViewModel/Commands/EndEditingCommand.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace EvernoteClone.ViewModel.Commands
 {
-    public class EndEditingCommand
+    public class EndEditingCommand : ICommand
     {
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
