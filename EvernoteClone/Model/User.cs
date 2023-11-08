2a0929fdfc3f7639c2674d1d605fe9940dc0a6a1
totarochristian <totaro.christian.18@gmail.com>
2023-11-08 10:43:20 +0100

    Remove sql max length of name and lastname in the User class

--- EvernoteClone/Model/User.cs
+++ EvernoteClone/Model/User.cs
@@ -11,9 +11,7 @@ namespace EvernoteClone.Model
     {
         [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
-        [MaxLength(50)]
         public string Name { get; set; }
-        [MaxLength(50)]
         public string Lastname { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
