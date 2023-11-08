6344886ee70dee99581a358a2d7bd982fd0f77f2
totarochristian <totaro.christian.18@gmail.com>
2023-11-08 17:37:29 +0100

    Update user class, remove the sqlite tags

--- EvernoteClone/Model/User.cs
+++ EvernoteClone/Model/User.cs
@@ -9,7 +9,6 @@ namespace EvernoteClone.Model
 {
     public class User
     {
-        [PrimaryKey, AutoIncrement]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Lastname { get; set; }
