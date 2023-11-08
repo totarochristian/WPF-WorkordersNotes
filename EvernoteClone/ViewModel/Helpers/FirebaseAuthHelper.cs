9f9f00ca0a7350c7f9d094851d250f5f0914603b
totarochristian <totaro.christian.18@gmail.com>
2023-11-08 15:29:41 +0100

    Update FirebaseAuthHelper class: add RegisterResult class

--- EvernoteClone/ViewModel/Helpers/FirebaseAuthHelper.cs
+++ EvernoteClone/ViewModel/Helpers/FirebaseAuthHelper.cs
@@ -40,5 +40,14 @@ namespace EvernoteClone.ViewModel.Helpers
                 }
             }
         }
+
+        public class RegisterResult
+        {
+            public string kind { get; set; }
+            public string idToken { get; set; }
+            public string email { get; set; }
+            public string refreshToken { get; set; }
+            public string expiresIn { get; set; }
+            public string localId { get; set; }
     }
 }
