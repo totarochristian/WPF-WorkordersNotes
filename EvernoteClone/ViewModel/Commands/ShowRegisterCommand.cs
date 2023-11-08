10a62fbca65c77867022ad7381ebb1faa971f5bd
totarochristian <totaro.christian.18@gmail.com>
2023-11-08 10:53:49 +0100

    Update ShowRegisterCommand, update CanExecute method returning always true

--- EvernoteClone/ViewModel/Commands/ShowRegisterCommand.cs
+++ EvernoteClone/ViewModel/Commands/ShowRegisterCommand.cs
@@ -20,7 +20,7 @@ namespace EvernoteClone.ViewModel.Commands
 
         public bool CanExecute(object? parameter)
         {
-            throw new NotImplementedException();
+            return true;
         }
 
         public void Execute(object? parameter)
