4aaf287352609e455a95e77e71bc16eec1ab79d2
totarochristian <totaro.christian.18@gmail.com>
2023-11-08 14:17:57 +0100

    Update FirebaseAuthHelper class, add the Register method (empty)

--- EvernoteClone/ViewModel/Helpers/FirebaseAuthHelper.cs
+++ EvernoteClone/ViewModel/Helpers/FirebaseAuthHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EvernoteClone.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,9 @@ namespace EvernoteClone.ViewModel.Helpers
     {
         private static string api_key = "AIzaSyDCViISbSPykopxo4RFARdVeLJLcApIS0M";
 
-
+        public static bool Register(User user)
+        {
+            return false;
+        }
     }
 }
