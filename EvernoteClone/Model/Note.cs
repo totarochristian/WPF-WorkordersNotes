133db429edafcfda3b7447c08aa2580bdb79c3f5
totarochristian <totaro.christian.18@gmail.com>
2023-11-08 17:35:52 +0100

    Update note class, set the notebook id to string instead of int

--- EvernoteClone/Model/Note.cs
+++ EvernoteClone/Model/Note.cs
@@ -10,7 +10,7 @@ namespace EvernoteClone.Model
     public class Note
     {
         public string Id { get; set; }
-        public int NotebookId { get; set; }
+        public string NotebookId { get; set; }
         public string Title { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
