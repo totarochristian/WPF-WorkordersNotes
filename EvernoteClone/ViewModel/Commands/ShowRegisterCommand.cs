7f424cb95775ec215204f58f34f688695ad03b1f
totarochristian <totaro.christian.18@gmail.com>
2023-11-08 10:53:07 +0100

    Add property LoginVM and the constructor with the login view model as param in the ShowRegisterCommand class

--- EvernoteClone/ViewModel/Commands/ShowRegisterCommand.cs
+++ EvernoteClone/ViewModel/Commands/ShowRegisterCommand.cs
@@ -9,7 +9,14 @@ namespace EvernoteClone.ViewModel.Commands
 {
     public class ShowRegisterCommand : ICommand
     {
+        public LoginVM VM { get; set; }
+
         public event EventHandler? CanExecuteChanged;
+
+        public ShowRegisterCommand(LoginVM vm) {
+
+            VM = vm;
+        }
 
         public bool CanExecute(object? parameter)
         {
