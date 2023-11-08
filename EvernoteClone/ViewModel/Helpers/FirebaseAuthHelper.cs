cbd3154b6bb6c243a439a191a998df4f072887c8
totarochristian <totaro.christian.18@gmail.com>
2023-11-08 15:46:43 +0100

    Update FirebaseAuthHelper, update the method Register: - add deserialization of the resul - add assignment of the local id as user id - if the response is not success, read the error response async - deserialize the error response using Error class - display a message box with the error message in case of errors - add returns
    Update FirebaseAuthHelper, add the following classes:
    - RegisterResult
    - ErrorDetails
    - Error

--- EvernoteClone/ViewModel/Helpers/FirebaseAuthHelper.cs
+++ EvernoteClone/ViewModel/Helpers/FirebaseAuthHelper.cs
@@ -7,6 +7,7 @@ using System.Net.Http;
 using System.Text;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace EvernoteClone.ViewModel.Helpers
 {
@@ -17,7 +18,7 @@ namespace EvernoteClone.ViewModel.Helpers
         public static async Task<bool> Register(User user)
         {
             //Init a new http client because this API use REST calls
-            using(HttpClient client = new HttpClient())
+            using (HttpClient client = new HttpClient())
             {
                 //Create a new anonimous object with username, password and a bool called returnSecureToken (mandatory)
                 var body = new
@@ -33,10 +34,25 @@ namespace EvernoteClone.ViewModel.Helpers
                 //Send the request via async post to a specific url and retrieve the result response
                 var response = await client.PostAsync($"https://identitytoolkit.googleapis.com/v1/accounts:signUp?key={api_key}", data);
                 //If request success
-                if(response.IsSuccessStatusCode)
+                if (response.IsSuccessStatusCode)
                 {
                     //Read async the content of the response as a string
                     string resultJson = await response.Content.ReadAsStringAsync();
+                    //Deserialize the result in json format using the class RegisterResult as "template" of the structure
+                    var result = JsonConvert.DeserializeObject<RegisterResult>(resultJson);
+                    //Save the local id in the result as user id
+                    App.UserId = result.localId;
+                    return true;
+                }
+                else
+                {
+                    //Read async the content of the error response as a string
+                    string errorJson = await response.Content.ReadAsStringAsync();
+                    //Deserialize the result in json format using the class Error as "template" of the structure
+                    var error = JsonConvert.DeserializeObject<Error>(errorJson);
+                    //Display the error message using a message box
+                    MessageBox.Show(error.error.message);
+                    return false;
                 }
             }
         }
@@ -49,5 +65,17 @@ namespace EvernoteClone.ViewModel.Helpers
             public string refreshToken { get; set; }
             public string expiresIn { get; set; }
             public string localId { get; set; }
+        }
+
+        public class ErrorDetails
+        {
+            public int code { get; set; }
+            public string message { get; set; }
+        }
+
+        public class Error
+        {
+            public ErrorDetails error { get; set; }
+        }
     }
 }
