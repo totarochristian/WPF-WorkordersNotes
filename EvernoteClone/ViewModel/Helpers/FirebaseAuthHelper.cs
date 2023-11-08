3388084684c23c9c0b51c4331c3c700a57d34aa7
totarochristian <totaro.christian.18@gmail.com>
2023-11-08 14:15:00 +0100

    Add FirebaseAuthHelper class, set it public and define the api key inside of it as a private string constant

---
+++ EvernoteClone/ViewModel/Helpers/FirebaseAuthHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvernoteClone.ViewModel.Helpers
+{
+    public class FirebaseAuthHelper
+    {
+        private const string api_key = "AIzaSyDCViISbSPykopxo4RFARdVeLJLcApIS0M";
+    }
+}
