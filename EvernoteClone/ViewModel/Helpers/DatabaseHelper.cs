a4d5a875e28b6a3ca701c1303ae76eb8bbeb65d0
totarochristian <totaro.christian.18@gmail.com>
2023-11-08 17:31:15 +0100

    Update DatabaseHelper, update the Insert method adding code to save in the firebase database instead of the local database

--- EvernoteClone/ViewModel/Helpers/DatabaseHelper.cs
+++ EvernoteClone/ViewModel/Helpers/DatabaseHelper.cs
@@ -1,8 +1,10 @@
-﻿using SQLite;
+﻿using Newtonsoft.Json;
+using SQLite;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -19,22 +21,40 @@ namespace EvernoteClone.ViewModel.Helpers
         /// <typeparam name="T">Type of the object to be inserted in the database</typeparam>
         /// <param name="item">Object to be inserted in the database</param>
         /// <returns></returns>
-        public static bool Insert<T>(T item)
+        public static async Task<bool> Insert<T>(T item)
         {
-            bool result = false;
+            //bool result = false;
 
-            //Connect to the database
-            using(SQLiteConnection conn = new SQLiteConnection(dbFile))
+            ////Connect to the database
+            //using(SQLiteConnection conn = new SQLiteConnection(dbFile))
+            //{
+            //    //Create a generic table in the database, if not exists
+            //    conn.CreateTable<T>();
+            //    //Insert the item in the database and save the rows affected
+            //    int rows = conn.Insert(item);
+            //    //If the number of rows affected is greater then 0, set result to true, otherwise false
+            //    result = rows > 0;
+            //}
+
+            //return result;
+
+            //Serialize the item passed as parameter making the body of the REST call that will be done to save data in the firebase database
+            var body = JsonConvert.SerializeObject(item);
+            //Define the content using the serialized object, the utf-8 encoding and the string "application/json" as media type
+            var content  = new StringContent(body, Encoding.UTF8, "application/json");
+            //initialize the http client
+            using(var client = new HttpClient())
             {
-                //Create a generic table in the database, if not exists
-                conn.CreateTable<T>();
-                //Insert the item in the database and save the rows affected
-                int rows = conn.Insert(item);
-                //If the number of rows affected is greater then 0, set result to true, otherwise false
-                result = rows > 0;
+                //Start the post request to the path of the database composed with the name of the item type
+                var result = await client.PostAsync($"{dbPath}{item.GetType().Name.ToLower()}.json", content);
+                //If the result is success
+                if(result.IsSuccessStatusCode)
+                {
+                    return true;
+                }else { 
+                    return false;
+                }
             }
-
-            return result;
         }
 
         /// <summary>
