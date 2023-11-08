ca8ed1700200c626fbf25b4425fa34131710d838
totarochristian <totaro.christian.18@gmail.com>
2023-11-08 11:08:00 +0100

    Update LoginVm: - add property LoginVisibility - update constructor adding the initialization of the propert LoginVisibility - extend the class with INotifyProeprtyChanged and implement the interface - add the method OnPropertyChanged to invoke the property changed events - update the setter of LoginVisibility property adding the call of the method OnPropertyChanged - add property RegisterVisibility - update constructor adding the initialization of the propert RegisterVisibility - update SwitchViews methos adding code to update the visibilities of login and register using the value of isShowingRegister

--- EvernoteClone/ViewModel/LoginVM.cs
+++ EvernoteClone/ViewModel/LoginVM.cs
@@ -2,13 +2,15 @@
 using EvernoteClone.ViewModel.Commands;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace EvernoteClone.ViewModel
 {
-    public class LoginVM
+    public class LoginVM : INotifyPropertyChanged
     {
 		private bool isShowingRegister = false;
 		private User user;
@@ -19,19 +21,57 @@ namespace EvernoteClone.ViewModel
 			set { user = value; }
 		}
 
-		public RegisterCommand RegisterCommand { get; set; }
+		private Visibility loginVisibility;
+        public Visibility LoginVisibility
+		{
+			get { return loginVisibility; }
+			set { 
+				loginVisibility = value;
+                //Call the event to change the login visibility in the list view
+                OnPropertyChanged("LoginVisibility");
+			}
+		}
+
+        private Visibility registerVisibility;
+        public Visibility RegisterVisibility
+        {
+            get { return registerVisibility; }
+            set
+            {
+                registerVisibility = value;
+                //Call the event to change the register visibility in the list view
+                OnPropertyChanged("RegisterVisibility");
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public RegisterCommand RegisterCommand { get; set; }
 		public LoginCommand LoginCommand { get; set; }
 
 		public LoginVM()
 		{
-			RegisterCommand = new RegisterCommand(this);
+            //Define the commands related to the user view model
+            RegisterCommand = new RegisterCommand(this);
 			LoginCommand = new LoginCommand(this);
-		}
 
-		public void SwitchViews()
+            //Define initial values
+			LoginVisibility = Visibility.Visible;
+			RegisterVisibility = Visibility.Collapsed;
+        }
+
+        public void SwitchViews()
 		{
 			//Switch the value
 			isShowingRegister = !isShowingRegister;
+			//Assign the visibility for each the visibilities property (login and register)
+			LoginVisibility = !isShowingRegister ? Visibility.Visible : Visibility.Collapsed;
+            RegisterVisibility = isShowingRegister ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+		private void OnPropertyChanged(string propertyName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
 		}
 	}
 }
