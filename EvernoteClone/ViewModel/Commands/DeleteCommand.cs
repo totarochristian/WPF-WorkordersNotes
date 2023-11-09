6d91b2ec3429ab4784893292e460cdaeee8a3472
totarochristian <totaro.christian.18@gmail.com>
2023-11-09 18:17:54 +0100

    Update DeleteCommand: - define view model property - add constructor with view model param - define execute method preparing the selection of the related delete method

--- EvernoteClone/ViewModel/Commands/DeleteCommand.cs
+++ EvernoteClone/ViewModel/Commands/DeleteCommand.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EvernoteClone.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,10 +10,17 @@ namespace EvernoteClone.ViewModel.Commands
 {
     public class DeleteCommand : ICommand
     {
+        public NotesVM VM { get; set; }
+
         public event EventHandler? CanExecuteChanged
         {
             add { CommandManager.RequerySuggested += value; }
             remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public DeleteCommand(NotesVM vm)
+        {
+            VM = vm;
         }
 
         public bool CanExecute(object? parameter)
@@ -22,7 +30,28 @@ namespace EvernoteClone.ViewModel.Commands
 
         public void Execute(object? parameter)
         {
-            throw new NotImplementedException();
+            if (parameter.GetType() == typeof(Notebook))
+            {
+                //Cast the parameter as Notebook object
+                Notebook notebook = parameter as Notebook;
+                //If the notebook retrieved isn't null
+                if (notebook != null)
+                {
+                    //Call the delete method of VM, deleting the casted notebook
+                    VM.DeleteNotebook(notebook);
+                }
+            }
+            else if (parameter.GetType() == typeof(Note))
+            {
+                //Cast the parameter as Note object
+                Note note = parameter as Note;
+                //If the note retrieved isn't null
+                if (note != null)
+                {
+                    //Call the delete method of VM, deleting the casted note
+                    VM.DeleteNote(notebook);
+                }
+            }
         }
     }
 }
