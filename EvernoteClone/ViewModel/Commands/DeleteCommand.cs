3670674b92b251120d92c7249005106497ab364e
totarochristian <totaro.christian.18@gmail.com>
2023-11-09 18:09:37 +0100

    Update delete command, extends ICommand and implement interface

--- EvernoteClone/ViewModel/Commands/DeleteCommand.cs
+++ EvernoteClone/ViewModel/Commands/DeleteCommand.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace EvernoteClone.ViewModel.Commands
 {
-    public class DeleteCommand
+    public class DeleteCommand : ICommand
     {
+        public event EventHandler? CanExecuteChanged;
+
+        public bool CanExecute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
