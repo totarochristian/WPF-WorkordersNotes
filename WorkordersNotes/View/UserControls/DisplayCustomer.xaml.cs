44bf3b885e615e9e86b0c88fa6fd952513ef4abc
totarochristian <totaro.christian.18@gmail.com>
2023-11-10 14:39:22 +0100

    Rename DisplayNotebook to DisplayCustomer

--- WorkordersNotes/View/UserControls/DisplayCustomer.xaml.cs
+++ WorkordersNotes/View/UserControls/DisplayCustomer.xaml.cs
@@ -17,9 +17,9 @@ using System.Windows.Shapes;
 namespace WorkordersNotes.View.UserControls
 {
     /// <summary>
-    /// Interaction logic for DisplayNotebook.xaml
+    /// Interaction logic for DisplayCustomer.xaml
     /// </summary>
-    public partial class DisplayNotebook : UserControl
+    public partial class DisplayCustomer : UserControl
     {
         public Customer Customer
         {
@@ -29,12 +29,12 @@ namespace WorkordersNotes.View.UserControls
 
         // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
         public static readonly DependencyProperty NotebookProperty =
-            DependencyProperty.Register("Customer", typeof(Customer), typeof(DisplayNotebook), new PropertyMetadata(null, SetValues));
+            DependencyProperty.Register("Customer", typeof(Customer), typeof(DisplayCustomer), new PropertyMetadata(null, SetValues));
 
         private static void SetValues(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             //Cast the dependency object to this type of user control
-            DisplayNotebook notebookUserControl = d as DisplayNotebook;
+            DisplayCustomer notebookUserControl = d as DisplayCustomer;
             //When change the property, re-assign it to the user control if is not null
             if(notebookUserControl != null)
             {
@@ -42,7 +42,7 @@ namespace WorkordersNotes.View.UserControls
             }
         }
 
-        public DisplayNotebook()
+        public DisplayCustomer()
         {
             InitializeComponent();
         }
