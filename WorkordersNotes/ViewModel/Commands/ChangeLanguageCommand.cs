4ff8d37e13cd7c9302b1581694b9d8c25a451ad7
totarochristian <totaro.christian.18@gmail.com>
2023-11-10 15:35:00 +0100

    Update ChangeLanguageCommand class: extend ICommand interface and then implement it

--- WorkordersNotes/ViewModel/Commands/ChangeLanguageCommand.cs
+++ WorkordersNotes/ViewModel/Commands/ChangeLanguageCommand.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace WorkordersNotes.ViewModel.Commands
 {
-    public class ChangeLanguageCommand
+    public class ChangeLanguageCommand : ICommand
     {
+        public event EventHandler? CanExecuteChanged;
 
+        public bool CanExecute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Execute(object? parameter)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
