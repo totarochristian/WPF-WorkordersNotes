50a3e8ad91e11bdfa300971d3e799c761166350a
totarochristian <totaro.christian.18@gmail.com>
2023-11-10 15:55:51 +0100

    Update ChangeLanguageCommand: - implement the execute method switching language selecting the correct dictionary - add dictionary as public property of the class - add language changed event, invoked in the execute method

--- WorkordersNotes/ViewModel/Commands/ChangeLanguageCommand.cs
+++ WorkordersNotes/ViewModel/Commands/ChangeLanguageCommand.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
 
 namespace WorkordersNotes.ViewModel.Commands
@@ -10,6 +11,10 @@ namespace WorkordersNotes.ViewModel.Commands
     public class ChangeLanguageCommand : ICommand
     {
         public NotesVM VM { get; set; }
+
+        public ResourceDictionary Dictionary { get; private set; }
+
+        public event EventHandler LanguageChanged;
 
         public event EventHandler? CanExecuteChanged
         {
@@ -29,7 +34,28 @@ namespace WorkordersNotes.ViewModel.Commands
 
         public void Execute(object? parameter)
         {
-            throw new NotImplementedException();
+            //Teorically the parameter is the language name es: Italian
+            string language = parameter as string;
+            Dictionary = new ResourceDictionary();
+            //If the language isn't null
+            if (language != null)
+            {
+                //Define the dictionary source using the language passed as parameter
+                switch(language)
+                {
+                    case "Italian":
+                        Dictionary.Source = new Uri("..\\StringResources.it.xaml", UriKind.Relative);
+                        break;
+                    case "English":
+                        Dictionary.Source = new Uri("..\\StringResources.it.xaml", UriKind.Relative);
+                        break;
+                    default:
+                        Dictionary.Source = new Uri("..\\StringResources.it.xaml", UriKind.Relative);
+                        break;
+                }
+                //Invoke the language changed event
+                LanguageChanged?.Invoke(this, new EventArgs()));
+            }
         }
     }
 }
